cf1412acc706b766aa6368213d943b471653a177
karim-manaouil <afr0ck.bin@gmail.com>
2019-07-07 03:44:41 +0200

    added density ordered algorithm ukp_dno()

--- ukp.py
+++ ukp.py
@@ -21,8 +21,14 @@ class rowtwin:
         self.row = row
         self.obj = obj
 
+class ukp_solution:
+    def __init__(self, objs, total):
+        self.total = total
+        self.objs = objs
+
 def ukp_dno(ukp_obj):
     twins = []
+    taken = []
     ukp_obj.validate()
 
     for i in range(0, len(ukp_obj.p)):
@@ -30,10 +36,35 @@ def ukp_dno(ukp_obj):
         twins.append(rowtwin(i, tmp_row))
 
     twins.sort(key=lambda x: x.row, reverse=True)
+    # for twin in twins:
+    #     print (str(twin.obj) + ":" + str(twin.row))
 
-    for twin in twins:
-        print (str(twin.obj) + ":" + str(twin.row))
+    current_capacity = ukp_obj.capacity
+    total_profit = 0
+    current_obj_i = 0
+    current_obj = twins[current_obj_i].obj
+    cont = True
+
+    while current_capacity > 0 and cont:
+        if (ukp_obj.w[current_obj] > current_capacity):
+            if ++current_obj_i < len(ukp_obj.p):
+                current_obj = twins[current_obj_i].obj
+                continue
+            else:
+                cont = False
+                continue
+
+        taken.append(current_obj)
+        total_profit += ukp_obj.p[current_obj]
+        current_capacity -= ukp_obj.w[current_obj]
+
+    return ukp_solution(taken, total_profit)
+
 
 # main
 instance = ukp(10, [5, 6, 7, 8], [1, 2, 3, 4])
-ukp_dno(instance)
+solution = ukp_dno(instance)
+
+print ("Total profit = " + str(solution.total))
+for obj in solution.objs:
+    print (str(obj) + " ")
