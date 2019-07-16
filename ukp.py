8f1508c70afcb8401f986fe4fe789191256de443
abdouTCH13 <fa_tayebcherif@esi.dz>
2019-07-16 18:00:23 +0100

    SIMULATED ANNEALING ADDED -_-

--- ukp.py
+++ ukp.py
@@ -358,6 +358,114 @@ def ukp_debinarize_solution (ukp_obj, sv):
 
 ############################# End of Genetic Algorithm #############################
 
+
+
+
+
+
+############################# simulated annealing algorithme #######################
+# coding: utf-8
+
+import math
+import random
+
+ALPHA = 0.85
+
+
+def ukp_binarize_simulated_annealing(ukp_obj):
+    weight_cost = ([], [])
+    temp = ukp_binarize(ukp_obj)
+
+    weight_cost = [(temp.w[i], temp.p[i]) for i in range(len(temp.w))]
+    return weight_cost
+
+
+def ukp_annealing_algorithm(number, capacity, weight_cost, init_temp=100, steps=100):
+    start_sol = init_solution(weight_cost, capacity)
+    best_cost, solution = simulate(start_sol, weight_cost, capacity, init_temp, steps)
+    best_combination = [0] * number
+    ukp_sol_o = ukp_solution()
+    for idx in solution:
+        best_combination[idx] = 1
+    return best_combination
+
+
+def init_solution(weight_cost, max_weight):
+    """Used for initial solution generation.
+    By adding a random item while weight is less max_weight
+    """
+    solution = []
+    allowed_positions = list(range(len(weight_cost)))
+    while len(allowed_positions) > 0:
+        idx = random.randint(0, len(allowed_positions) - 1)
+        selected_position = allowed_positions.pop(idx)
+        if get_cost_and_weight_of_knapsack(solution + [selected_position], weight_cost)[1] <= max_weight:
+            solution.append(selected_position)
+        else:
+            break
+    return solution
+
+
+def get_cost_and_weight_of_knapsack(solution, weight_cost):
+    """Get cost and weight of knapsack - fitness function
+    """
+    cost, weight = 0, 0
+    for item in solution:
+        weight += weight_cost[item][0]
+        cost += weight_cost[item][1]
+    return cost, weight
+
+
+def moveto(solution, weight_cost, max_weight):
+    """All possible moves are generated"""
+    moves = []
+    for idx, _ in enumerate(weight_cost):
+        if idx not in solution:
+            move = solution[:]
+            move.append(idx)
+            if get_cost_and_weight_of_knapsack(move, weight_cost)[1] <= max_weight:
+                moves.append(move)
+    for idx, _ in enumerate(solution):
+        move = solution[:]
+        del move[idx]
+        if move not in moves:
+            moves.append(move)
+    return moves
+
+
+def simulate(solution, weight_cost, max_weight, init_temp, steps):
+    """Simulated annealing approach for Knapsack problem"""
+    temperature = init_temp
+
+    best = solution
+    best_cost = get_cost_and_weight_of_knapsack(solution, weight_cost)[0]
+
+    current_sol = solution
+    while True:
+        current_cost = get_cost_and_weight_of_knapsack(best, weight_cost)[0]
+        for i in range(0, steps):
+            moves = moveto(current_sol, weight_cost, max_weight)
+            idx = random.randint(0, len(moves) - 1)
+            random_move = moves[idx]
+            delta = get_cost_and_weight_of_knapsack(random_move, weight_cost)[0] - best_cost
+            if delta > 0:
+                best = random_move
+                best_cost = get_cost_and_weight_of_knapsack(best, weight_cost)[0]
+                current_sol = random_move
+            else:
+                if math.exp(delta / float(temperature)) > random.random():
+                    current_sol = random_move
+
+        temperature *= ALPHA
+        if current_cost >= best_cost or temperature <= 0:
+            break
+    return best_cost, best
+
+
+######################## end of simulated annealing algorithme ####################
+
+
+
 def read_benchmark_instance(path):
     ukp_obj = ukp(0, [], [])
 
@@ -405,8 +513,15 @@ def execute_instance(*k):
         ukp_o_bin = ukp_binarize(ukp_o)
         selected = ukp_ga(ukp_o, ukp_o_bin, k[2], k[3])
         solution = ukp_debinarize_solution(ukp_o_bin, selected)
+    elif type == "ukp_sa":
+        ukp_bin = ukp_binarize(ukp_o)
+        weight_coast_vector = ukp_binarize_simulated_annealing(ukp_o)
+        selected = ukp_annealing_algorithm(len(weight_coast_vector), ukp_o.capacity, weight_coast_vector,\
+                                           SA_PARAMETERS.TEMPERATURE,SA_PARAMETERS.NBRE_CYCLES)
+        solution = ukp_debinarize_solution(ukp_bin, selected)
 
     else:
+
         return
 
     time = str(timedelta(seconds=timer() - start))
@@ -416,7 +531,7 @@ def execute_instance(*k):
     if global_fhandle != 0:
         global_fhandle.write(type + ',' + time + ',' + str(solution.total) + ',' + str(solution.tw) + '\n')
 
-    #print (type + ',' + time + ',' + str(solution.total) + ',' + str(solution.tw))
+    print (type + ',' + time + ',' + str(solution.total) + ',' + str(solution.tw))
 
     return {"sol":solution, "time":time}
 
@@ -434,12 +549,14 @@ def do_benchmark(file, fhandle):
     execute_instance("ukp_wo", ukp_obj)
     execute_instance("ukp_dno", ukp_obj)
     execute_instance("ukp_tv", ukp_obj)
+    execute_instance("ukp_sa", ukp_obj)
     execute_instance("ukp_ga.10.20", ukp_obj, 10, 0.20)
     execute_instance("ukp_ga.100.20", ukp_obj, 100, 0.20)
     execute_instance("ukp_ga.1000.20", ukp_obj, 1000, 0.20)
     execute_instance("ukp_ga.100.60", ukp_obj, 100, 0.60)
     execute_instance("ukp_ga.1000.60", ukp_obj, 1000, 0.60)
     execute_instance("ukp_ga.1000.10", ukp_obj, 1000, 0.10)
+
 
 def run_bechmarks(path, tmp):
     for r, d, f in os.walk(path):
@@ -453,6 +570,11 @@ def run_bechmarks(path, tmp):
 class GA_PARAMETERS:
     GENERATIONS = 1000
     MUTATION_RATIO = 0.10
+
+
+class SA_PARAMETERS:
+    TEMPERATURE = 100
+    NBRE_CYCLES = 100
 
 def main():
     assets = [
